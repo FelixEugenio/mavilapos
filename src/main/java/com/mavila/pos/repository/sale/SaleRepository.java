19723b45117ea749ea8aa5c62b8d007c9df1c71e
felix eugenio mavila <felixmacarenco@gmail.com>
2026-05-24 13:07:23 +0100

    feat: add SaleRepository

--- src/main/java/com/mavila/pos/repository/sale/SaleRepository.java
+++ src/main/java/com/mavila/pos/repository/sale/SaleRepository.java
@@ -1,10 +1,30 @@
 package com.mavila.pos.repository.sale;
 
 import com.mavila.pos.entity.Sale.Sale;
+import com.mavila.pos.entity.Sale.enums.SaleStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Repository
 public interface SaleRepository extends JpaRepository<Sale, Long> {
+    List<Sale> findByStatus(SaleStatus status);
 
+    List<Sale> findByCreatedAtBetween(
+            LocalDateTime start,
+            LocalDateTime end
+    );
+
+    @Query("""
+            SELECT COALESCE(SUM(s.total), 0)
+            FROM Sale s
+            WHERE DATE(s.createdAt) = :date
+            AND s.status = 'COMPLETED'
+            """)
+    BigDecimal getTotalSalesByDate(LocalDate date);
 }
