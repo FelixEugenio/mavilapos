8cf34232cb74d0f3255959e8cea2f0c8ce338ff0
felix eugenio mavila <felixmacarenco@gmail.com>
2026-05-24 12:08:37 +0100

    feat: add ProductMapper

--- src/main/java/com/mavila/pos/mapper/ProductMapper.java
+++ src/main/java/com/mavila/pos/mapper/ProductMapper.java
@@ -1,4 +1,20 @@
 package com.mavila.pos.mapper;
 
-public class ProductMapper {
+import com.mavila.pos.config.MapStructConfig;
+import com.mavila.pos.dto.request.ProductRequestDTO;
+import com.mavila.pos.dto.response.ProductResponseDTO;
+import com.mavila.pos.entity.product.Product;
+import org.mapstruct.Mapper;
+import org.mapstruct.MapperConfig;
+import org.mapstruct.Mapping;
+
+@Mapper(config = MapStructConfig.class)
+public interface ProductMapper {
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "category", ignore = true)
+    @Mapping(target = "active", constant = "true")
+    Product toEntity(ProductRequestDTO dto);
+
+    @Mapping(target = "category", source = "category.name")
+    ProductResponseDTO toResponse(Product entity);
 }
