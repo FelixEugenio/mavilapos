4c079e6a9755b58568080c542f2ab8ca471ad8b9
felix eugenio mavila <felixmacarenco@gmail.com>
2026-05-24 12:39:32 +0100

    feat: add cashMovementMapper

--- src/main/java/com/mavila/pos/mapper/CashMovementMapper.java
+++ src/main/java/com/mavila/pos/mapper/CashMovementMapper.java
@@ -1,4 +1,12 @@
 package com.mavila.pos.mapper;
 
-public class CashMovementMapper {
+import com.mavila.pos.config.MapStructConfig;
+import com.mavila.pos.dto.response.CashMovementResponseDTO;
+import com.mavila.pos.entity.cashMovement.cashMovement;
+import org.mapstruct.Mapper;
+
+@Mapper(config = MapStructConfig.class)
+public interface CashMovementMapper {
+
+    CashMovementResponseDTO toResponse(cashMovement entity);;
 }
