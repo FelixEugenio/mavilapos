915e6804d919a4370fd9d5a75c0353063b868e05
felix eugenio mavila <felixmacarenco@gmail.com>
2026-05-24 12:01:19 +0100

    feat: add CategoryMapper

--- src/main/java/com/mavila/pos/mapper/CategoryMapper.java
+++ src/main/java/com/mavila/pos/mapper/CategoryMapper.java
@@ -1,4 +1,13 @@
 package com.mavila.pos.mapper;
 
-public class CategoryMapper {
+import com.mavila.pos.config.MapStructConfig;
+import com.mavila.pos.dto.request.CategoryRequestDTO;
+import com.mavila.pos.dto.response.CategoryResponseDTO;
+import com.mavila.pos.entity.category.Category;
+import org.mapstruct.Mapper;
+
+@Mapper(config = MapStructConfig.class)
+public interface CategoryMapper {
+    Category toEntity(CategoryRequestDTO dto);
+    CategoryResponseDTO toResponseDTO(Category entity);
 }
