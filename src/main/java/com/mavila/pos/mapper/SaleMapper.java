e7c300d4c8f231d199bac3c3b12bba1a800dc4a9
felix eugenio mavila <felixmacarenco@gmail.com>
2026-05-24 12:25:35 +0100

    feat: add SaleMapper

--- src/main/java/com/mavila/pos/mapper/SaleMapper.java
+++ src/main/java/com/mavila/pos/mapper/SaleMapper.java
@@ -1,4 +1,18 @@
 package com.mavila.pos.mapper;
 
-public class SaleMapper {
+import com.mavila.pos.config.MapStructConfig;
+import com.mavila.pos.dto.response.SaleItemResponseDTO;
+import com.mavila.pos.dto.response.SaleResponseDTO;
+import com.mavila.pos.entity.Sale.Sale;
+import com.mavila.pos.entity.SaleItem.SaleItem;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(config = MapStructConfig.class)
+public interface SaleMapper {
+    @Mapping(target = "cashier", source = "cashier.name")
+    SaleResponseDTO toResponse(Sale entity);
+
+    @Mapping(target = "product", source = "product.name")
+    SaleItemResponseDTO toItemResponse(SaleItem entity);
 }
