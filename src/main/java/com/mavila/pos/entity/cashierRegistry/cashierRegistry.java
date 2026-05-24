79af98b2e44c49373a0037efb6c6aea0e4ba6d35
felix eugenio mavila <felixmacarenco@gmail.com>
2026-05-24 11:23:51 +0100

    feat: add cashierRegistry

---
+++ src/main/java/com/mavila/pos/entity/cashierRegistry/cashierRegistry.java
@@ -0,0 +1,39 @@
+package com.mavila.pos.entity.cashierRegistry;
+
+import com.mavila.pos.entity.cashierRegistry.enums.CashierStatus;
+import com.mavila.pos.entity.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class cashierRegistry implements java.io.Serializable{
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDateTime openedAt;
+
+    private LocalDateTime closedAt;
+
+    private BigDecimal openingAmount;
+
+    private BigDecimal closingAmount;
+
+    @Enumerated(EnumType.STRING)
+    private CashierStatus status;
+
+    @ManyToOne
+    private User openedBy;
+}
---
+++ src/main/java/com/mavila/pos/entity/cashierRegistry/enums/CashierStatus.java
@@ -0,0 +1,6 @@
+package com.mavila.pos.entity.cashierRegistry.enums;
+
+public enum CashierStatus {
+    OPEN,
+    CLOSE
+}
