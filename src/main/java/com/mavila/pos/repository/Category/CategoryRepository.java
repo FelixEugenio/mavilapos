36d10a6894060dbf69352e55e58820d29f9064b2
felix eugenio mavila <felixmacarenco@gmail.com>
2026-05-24 13:03:16 +0100

    feat: add CategoryRepository

---
+++ src/main/java/com/mavila/pos/repository/Category/CategoryRepository.java
@@ -0,0 +1,11 @@
+package com.mavila.pos.repository.Category;
+
+import com.mavila.pos.entity.category.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+    Category findByName(String name);
+    Boolean existsByNameIgnoreCase(String name);
+}
