256d452a7f158ad74d6bd6c7f2edfb1ec962bc8e
felix eugenio mavila <felixmacarenco@gmail.com>
2026-05-24 12:14:06 +0100

    feat: add UserMapper

--- src/main/java/com/mavila/pos/mapper/UserMapper.java
+++ src/main/java/com/mavila/pos/mapper/UserMapper.java
@@ -1,8 +1,18 @@
 package com.mavila.pos.mapper;
 
+import com.mavila.pos.config.MapStructConfig;
+import com.mavila.pos.dto.request.UserRequestDTO;
+import com.mavila.pos.dto.response.UserResponseDTO;
+import com.mavila.pos.entity.user.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import tools.jackson.databind.cfg.MapperConfig;
 
-@Mapper(componentModel = "spring")
-public class UserMapper {
+@Mapper(config = MapStructConfig.class)
+public interface UserMapper {
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "active", constant = "true")
+    User toEntity(UserRequestDTO dto);
 
+    UserResponseDTO toResponse(User entity);
 }
