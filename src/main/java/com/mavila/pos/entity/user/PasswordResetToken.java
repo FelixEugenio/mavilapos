045086d622dca15d7a709148bf1fc79e4f2bffae
felix eugenio mavila <felixmacarenco@gmail.com>
2026-05-24 18:28:27 +0100

    feat: add PasswordResetTokenRepository

---
+++ src/main/java/com/mavila/pos/entity/user/PasswordResetToken.java
@@ -0,0 +1,29 @@
+package com.mavila.pos.entity.user;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "password_reset_tokens")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PasswordResetToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String token;
+
+    @OneToOne
+    private User user;
+
+    private LocalDateTime expiresAt;
+
+    private Boolean used = false;
+}
---
+++ src/main/java/com/mavila/pos/service/email/EmailService.java
@@ -0,0 +1,8 @@
+package com.mavila.pos.service.email;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+}
